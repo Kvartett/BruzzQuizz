c1d190665bc3818ad0f8156c04ca4a2589a95812
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2022-09-01 18:45:50 -0300

    função para coletar os dados do quizz e layout tela 3.2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-/*function getApiQuizz(){
+function getApiQuizz(){
     let promisse = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
     promisse.then(putApiQuizz)
     promisse.catch()
@@ -21,18 +21,48 @@ function putApiQuizz(answer){
     </li>`
     }
 }
-getApiQuizz()*/
+getApiQuizz()
 
 function createQuizz(){           /*collects the values needed for the creation of the Quizz*/
-    const title = document.getElementById('title').value;
-    const image = document.getElementById('image').value;
+    const title = `${document.getElementById('title').value}`;
+    const image = `${document.getElementById('image').value}`;
     const numberQuestions = document.getElementById('questions').value;
     const levels = document.getElementById('levels').value;
     const screen3 = document.querySelector('.screen31');
+    if (title.length < 20 || title.length > 65){  /*verifications*/
+        falhou();
+        window.location.reload();
+    }
+    if (checkUrl(image)){
+        falhou();
+        window.location.reload();
+    }
+    if (numberQuestions < 3){
+        falhou();
+        window.location.reload();
+    }
+    if (levels < 2){
+        falhou();
+        window.location.reload();
+    }
     screen3.classList.add('hide');
     questionsQuizz(title,image,numberQuestions,levels); 
 }
 
+let falhou = () => alert('Por favor, preencha os dados corretamente');
+
+function checkUrl(string) {
+    try {
+     let url = new URL(string)
+     return false
+   } catch(err) {
+       return true
+   }
+ }
+
+function questionsQuizz(title,image,numberQuestions,levels){
+    console.log(title);
+}
 
 
 
