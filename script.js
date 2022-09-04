3cd6ccae8ec28d857855f13448326f76332a1e88
Luiz <felipedevluiz@gmail.com>
2022-09-04 17:29:57 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 function getApiQuizz(){
     let promisse = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
+    // promisse.then(putApiQuizz) **correto
     promisse.then(putApiQuizz)
     promisse.catch()
 }
+
 let quizzapi;
 function putApiQuizz(answer){
     quizzapi = answer.data
@@ -18,10 +20,70 @@ function putApiQuizz(answer){
         </div>   
             <img src=${quizzapi[i].image}>
         
-    </li>`
+        </li>`
     }
+    showQuizz(answer);
 }
-getApiQuizz()
+getApiQuizz();
+
+let quizzSelected;
+function showQuizz(quizz) {
+
+    quizzSelected = quizz.data[30] // Alterar "quizzapi[0]", colocado para implementar função.
+    const showQuizzTitle = quizzSelected.title;
+    const showQuizzImage = quizzSelected.image;
+
+    const screen = document.querySelector(".screen2");
+    screen.innerHTML = "";
+    
+    const showScreen = `
+    <div class="choice1">
+        <img src="${showQuizzImage}">
+        <div>${showQuizzTitle}</div>
+    </div>
+    <div class="quizz">
+        <div class="quizz-header">${quizzSelected.questions.title[1]}[</div>
+        <div class="quizz-content">
+            <div class="picture">
+                <img src=$"${quizzSelected.questions.answers[1].image[1]}">
+                <div>${quizzSelected.questions.answers[1].text[1]}</div>
+            </div>
+            <div class="picture">
+                <img src=$"${quizzSelected.questions.answers[1].image[2]}">
+                <div>${quizzSelected.questions.answers[1].text[2]}</div>
+            </div>
+            <div class="picture">
+                <img src=$"${quizzSelected.questions.answers[1].image[3]}">
+                <div>${quizzSelected.questions.answers[1].text[3]}</div>
+            </div>
+            <div class="picture">
+                <img src=$"${quizzSelected.questions.answers[1].image[4]}">
+                <div>${quizzSelected.questions.answers[1].text[4]}</div>
+            </div>
+        </div>
+        <div class="quizz-header">${quizzSelected.questions.title[2]}</div>
+        <div class="quizz-content">
+            <div class="picture">
+                <img src=$"${quizzSelected.questions.answers[2].image[1]}">
+                <div>${quizzSelected.questions.answers[2].text[1]}</div>
+            </div>
+            <div class="picture">
+                <img src=$"${quizzSelected.questions.answers[2].image[2]}">
+                <div>${quizzSelected.questions.answers[2].text[2]}</div>
+            </div>
+            <div class="picture">
+                <img src=$"${quizzSelected.questions.answers[2].image[3]}">
+                <div>${quizzSelected.questions.answers[2].text[3]}</div>
+            </div>
+            <div class="picture">
+                <img src=$"${quizzSelected.questions.answers[2].image[4]}">
+                <div>${quizzSelected.questions.answers[2].text[4]}</div>
+            </div>
+        </div>
+    </div>`;
+
+    screen = showScreen;
+}
 
 function createQuizz(){           /*collects the values needed for the creation of the Quizz*/
     const title = `${document.getElementById('title').value}`;
